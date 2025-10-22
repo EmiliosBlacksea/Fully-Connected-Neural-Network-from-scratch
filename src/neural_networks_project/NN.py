c5d6f673c8aafc7e8adf104e8681cc321cc413b0
emilios <emiliosblacksea@gmail.com>
2025-10-22 13:34:48 +0300

    Added (Class Centroid) classifier

--- src/neural_networks_project/NN.py
+++ src/neural_networks_project/NN.py
@@ -1,11 +1,7 @@
 from functions.load_data import Load_data
-from functions.find_3_nn import Find_3_nn
+from math import inf
 import numpy as np
 import time
-
-import os
-from concurrent.futures import ThreadPoolExecutor, as_completed
-
 
 def NN():
 
@@ -17,8 +13,7 @@ def NN():
     labels_train,
     data_train
     ) = Load_data()
-
-    nn3_distances_labels = []    
+   
     log_nn = [] 
     log_nn3 = []
     repetitions = len(data_train)
@@ -33,57 +28,27 @@ def NN():
         all_nn.sort(key=lambda x:x[0])
     
         __time = time.time() - __time 
-        print(f'Image {i} took {__time:.2f} seconds to calculate')
         if all_nn[1][1] == all_nn[2][1]:
             nnof = all_nn[1][1]
         else:
             nnof = all_nn[0][1]
         log_nn3.append(nnof == labels_train[i])
         log_nn.append(all_nn[0][1] == labels_train[i])
+        print(f'Image {i} {__time:.2f}s, 1nn {log_nn[i]} 3nn {log_nn3[i]}' )
     
-    ##
-    #Thread attempt
-    ##
-    # nn3_distances_labels = []
-    # log_nn = []
-    # log_nn3 = []
-    # repetitions = 32
-
-    # start_time = time.time()
-
-    # def compute_one(i):
-    #     t0 = time.time()
-    #     nn_1 = Find_3_nn(data_train[i], data_batch_1, labels_batch_1)
-    #     nn_2 = Find_3_nn(data_train[i], data_batch_2, labels_batch_2)
-    #     nn_3 = Find_3_nn(data_train[i], data_batch_3, labels_batch_3)
-    #     nn_4 = Find_3_nn(data_train[i], data_batch_4, labels_batch_4)
-    #     nn_5 = Find_3_nn(data_train[i], data_batch_5, labels_batch_5)
-    #     all_nn = nn_1 + nn_2 + nn_3 + nn_4 + nn_5
-    #     all_nn.sort(key=lambda x: x[0])
-    #     elapsed = time.time() - t0
-
-    #     # 3-NN vote (your rule) and 1-NN check
-    #     nnof = all_nn[1][1] if all_nn[1][1] == all_nn[2][1] else all_nn[0][1]
-    #     ok3 = (nnof == labels_train[i])
-    #     ok1 = (all_nn[0][1] == labels_train[i])
-    #     return i, elapsed, ok3, ok1
-
-    # log_nn3 = [False] * repetitions
-    # log_nn = [False] * repetitions
-
-    # with ThreadPoolExecutor(32) as ex: #max_workers=os.cpu_count()
-    #     futures = [ex.submit(compute_one, i) for i in range(repetitions)]
-    #     for fut in as_completed(futures):
-    #         i, elapsed, ok3, ok1 = fut.result()
-    #         log_nn3[i] = ok3
-    #         log_nn[i] = ok1
-    #         print(f"Image {i} took {elapsed:.2f} seconds to calculate")
-
-
-    # ###
-    # ###
-
     nn3stats = np.mean(log_nn3)
     nnstats = np.mean(log_nn)
     print(time.time() - _time)
-    return nn3stats,nnstats, time.time() - _time
+    return nn3stats,nnstats, time.time() - _time, log_nn, log_nn3
+
+def Find_3_nn(test_data, data_batch, labels_batch):
+    distances_labels = [[inf, -1], [inf, -1], [inf, -1]]
+    for i in range(len(data_batch)):
+        cur = distances_labels[2][0]
+        pot = np.sum((test_data - data_batch[i]) ** 2) # Withought sqrt I am comparing distances ^ 2 to distances ^ 2 so closest 3 n withh still be the same
+        if pot < cur:
+            distances_labels[2][0] = pot
+            distances_labels[2][1] = labels_batch[i]
+            distances_labels.sort(key=lambda x: x[0])
+    return distances_labels
+
--- src/neural_networks_project/functions/load_data.py
+++ src/neural_networks_project/functions/load_data.py
@@ -4,20 +4,20 @@ import numpy as np
 #RETURNS labels_batch_1->5, data_batch_1->5, labels_training, data_training
 def Load_data():
     #Get labels for "training" images
-    labels_batch_1 = np.array(Unpickle('src/neural_networks_project/rawdata/data_batch_1')[b'labels'])
-    labels_batch_2 = np.array(Unpickle('src/neural_networks_project/rawdata/data_batch_2')[b'labels'])
-    labels_batch_3 = np.array(Unpickle('src/neural_networks_project/rawdata/data_batch_3')[b'labels'])
-    labels_batch_4 = np.array(Unpickle('src/neural_networks_project/rawdata/data_batch_4')[b'labels'])
-    labels_batch_5 = np.array(Unpickle('src/neural_networks_project/rawdata/data_batch_5')[b'labels'])
+    labels_batch_1 = np.array(Unpickle('src/neural_networks_project/rawdata/data_batch_1')[b'labels']).astype(int)
+    labels_batch_2 = np.array(Unpickle('src/neural_networks_project/rawdata/data_batch_2')[b'labels']).astype(int)
+    labels_batch_3 = np.array(Unpickle('src/neural_networks_project/rawdata/data_batch_3')[b'labels']).astype(int)
+    labels_batch_4 = np.array(Unpickle('src/neural_networks_project/rawdata/data_batch_4')[b'labels']).astype(int)
+    labels_batch_5 = np.array(Unpickle('src/neural_networks_project/rawdata/data_batch_5')[b'labels']).astype(int)
     #Get data for "training" images
-    data_batch_1 = np.array(Unpickle('src/neural_networks_project/rawdata/data_batch_1')[b'data'])
-    data_batch_2 = np.array(Unpickle('src/neural_networks_project/rawdata/data_batch_2')[b'data'])
-    data_batch_3 = np.array(Unpickle('src/neural_networks_project/rawdata/data_batch_3')[b'data'])
-    data_batch_4 = np.array(Unpickle('src/neural_networks_project/rawdata/data_batch_4')[b'data'])
-    data_batch_5 = np.array(Unpickle('src/neural_networks_project/rawdata/data_batch_5')[b'data'])
+    data_batch_1 = np.array(Unpickle('src/neural_networks_project/rawdata/data_batch_1')[b'data']).astype(int)
+    data_batch_2 = np.array(Unpickle('src/neural_networks_project/rawdata/data_batch_2')[b'data']).astype(int)
+    data_batch_3 = np.array(Unpickle('src/neural_networks_project/rawdata/data_batch_3')[b'data']).astype(int)
+    data_batch_4 = np.array(Unpickle('src/neural_networks_project/rawdata/data_batch_4')[b'data']).astype(int)
+    data_batch_5 = np.array(Unpickle('src/neural_networks_project/rawdata/data_batch_5')[b'data']).astype(int)
     #Get labels and datafor testing images
-    labels_testing = np.array(Unpickle('src/neural_networks_project/rawdata/test_batch')[b'labels'])
-    data_testing = np.array(Unpickle('src/neural_networks_project/rawdata/test_batch')[b'data'])
+    labels_testing = np.array(Unpickle('src/neural_networks_project/rawdata/test_batch')[b'labels']).astype(int)
+    data_testing = np.array(Unpickle('src/neural_networks_project/rawdata/test_batch')[b'data']).astype(int)
     
     return (
             labels_batch_1, labels_batch_2, labels_batch_3, labels_batch_4, labels_batch_5,
