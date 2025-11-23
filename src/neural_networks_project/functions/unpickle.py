ed99604e20f4858bf81b21183c9a310554318047
AIMIILIOS MAVROTHALASSITIS <139469340+EmiliosBlacksea@users.noreply.github.com>
2025-11-23 22:04:10 +0200

    Delete src/neural_networks_project directory

--- src/neural_networks_project/functions/unpickle.py
+++
@@ -1,7 +0,0 @@
-import pickle
-
-def Unpickle(file):
-    """Load CIFAR-10 batch file."""
-    with open(file, 'rb') as fo:
-        data_dict = pickle.load(fo, encoding='bytes')
-    return data_dict
